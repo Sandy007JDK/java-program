7bc272904cfddde57685d6bb4b949707acd79c16
Sandy007JDK <nanthakumarselvankvp@gmail.com>
2024-11-15 19:32:13 +0530

    Create assignment operator.java

---
+++ assignment operator.java
@@ -0,0 +1,31 @@
+package AllProgram;
+
+public class AssignmentOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num=10;
+String name="Java";
+System.out.println("num"+num);
+System.out.println("name"+name);
+//+= operator
+int a=10;
+a+=10;//a=a+10
+System.out.println("Value of a"+a);
+
+// &&(and) operator >-greater
+System.out.println("&&-And operator");
+System.out.println((5>3) && (8>5));
+//&&(and) operator <-lesser
+System.out.println((5<3) && (8<5));
+
+// || (and) operator >-greater
+System.out.println("||-Or operator");
+System.out.println((5<3) || (8<5));
+
+//! Not operator
+System.out.println("!-Not operator");
+System.out.println(!(5<3));
+	}
+
+}
