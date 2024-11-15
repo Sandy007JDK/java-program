732a878793111114077041ad26ab0c96e95ad589
Sandy007JDK <nanthakumarselvankvp@gmail.com>
2024-11-15 19:58:07 +0530

    Create Task Method.java

---
+++ Task Method.java
@@ -0,0 +1,15 @@
+package AllProgram;
+
+public class TaskMethod {
+
+		static void add(int a,int b) {
+
+		add(a,b);
+
+		}public static void main(String [] args)
+			{
+				int a=5,b=10;
+			int sum=a+b;
+		System.out.println("Answer"+sum);
+	}
+	}
