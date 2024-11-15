ee3441a7d2a7090bd63d84b5559d3fdce614270b
Sandy007JDK <nanthakumarselvankvp@gmail.com>
2024-11-15 19:51:22 +0530

    Create operator.java

---
+++ operator.java
@@ -0,0 +1,18 @@
+package AllProgram;
+
+public class Operator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//operator,+,-,*,%--reminder,/-Quotient
+//operator--a+b,+ perform the task and returns the value
+		//operand--a,b--just a variable to store the value 
+		
+		int a=5,b=10,c;
+		c=a+b;
+		System.out.println("The value of c"+c);
+//write a program to print the subtraction,multiplication,division
+		//and modulus value using Scanner class(Min two Var)
+	}
+
+}
