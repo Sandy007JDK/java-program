464b04ee06f81e98c51f7b023b2f1e3e76b50276
Sandy007JDK <nanthakumarselvankvp@gmail.com>
2024-11-15 19:59:21 +0530

    Create Task Method Return.java

---
+++ Task Method Return.java
@@ -0,0 +1,15 @@
+package AllProgram;
+
+public class TaskMethodReturn {
+	
+		static int addnum(int a,int b) {
+
+		return a+b;
+
+		}public static void main(String [] args)
+			{
+				int a=5,b=10;
+			int res=addnum(a,b);
+		System.out.println("Answer"+res);
+	}
+	}
